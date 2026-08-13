ed7f13c1085a7cf0dd79b8dde2e4886933c3d335
Jakub Bielecki <kuba.bielecki@outlook.com>
2026-08-13 15:33:28 +0200

    chore: Bump the project version to 0.7.2

--- imx_camera_toolkit/__init__.py
+++ imx_camera_toolkit/__init__.py
@@ -76,7 +76,7 @@ if TYPE_CHECKING:
     from .preview import preview as preview
     from .preview import serve as serve
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 __all__ = [
     "Camera",
